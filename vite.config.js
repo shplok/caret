aef3a68d14c639cbd4894651cf9f46075bf9914f
caret <caret@local>
2026-08-16 20:06:24 +0000

    set vite base and add github pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/caret/', // served from https://shplok.github.io/caret/
   plugins: [react()],
   server: {
     host: true,
